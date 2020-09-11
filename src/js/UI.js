2c8c927a53145cfc0010f840b8d584d7192801a9
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-11 14:26:18 +0200

    make levels btn programatic

--- src/js/UI.js
+++ src/js/UI.js
@@ -1,5 +1,6 @@
 
 import GameControl from './GameControl.js';
+import params from '../data/params.js';
 
 //
 
@@ -9,6 +10,8 @@ const domGameUI = document.querySelector('#game-ui');
 const domLevelBtns = document.querySelectorAll('.level-btn');
 const domLevelMenu = document.querySelector('#level-menu');
 const domWinMessage = document.querySelector('#win-message');
+const domFirstLevels = document.querySelector('#first-levels');
+const domSecondLevels = document.querySelector('#second-levels');
 
 const firstLevelsButtons = [];
 const secondLevelsButtons = [];
@@ -20,6 +23,13 @@ domStartBtn.addEventListener('click', () => {
 	GameControl.startNewGame();
 
 });
+
+
+
+
+
+
+/*
 
 // add level buttons to arrays and sort those arrays
 
@@ -44,6 +54,34 @@ function sortButtons( a, b ) {
 	if ( Number( a.id[0] ) > Number( b.id[0] ) ) return 1
 	else return -1
 };
+
+*/
+
+
+for ( let i=0 ; i<params.LEVELS_PER_FAMILY ; i++ ) {
+
+	const domFirstBtn = document.createElement('DIV');
+	domFirstBtn.innerHTML = i + 1;
+	domFirstBtn.classList.add('level-btn', String(i+1) );
+
+	domFirstLevels.append( domFirstBtn );
+
+	firstLevelsButtons.push( domFirstBtn );
+
+	//
+
+	const domSecondBtn = document.createElement('DIV');
+	domSecondBtn.innerHTML = i + 1;
+	domSecondBtn.classList.add('level-btn', i+1 + '-2' );
+
+	domSecondLevels.append( domSecondBtn );
+
+	secondLevelsButtons.push( domSecondBtn );
+
+}
+
+
+
 
 // add event listener on buttons to start levels on click
 // + add them to the 'disabled' class but the first one

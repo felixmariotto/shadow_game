89659e4483c5d15e4f75bfbfca26621104d68dbb
felixmariotto <46470486+felixmariotto@users.noreply.github.com>
2020-09-11 11:13:29 +0200

    smooth player moves

--- src/js/LevelControl.js
+++ src/js/LevelControl.js
@@ -15,6 +15,12 @@ Scene.add( worldGroup );
 
 let player;
 
+const clock = new THREE.Clock();
+
+const playerVelocity = new THREE.Vector3();
+
+const TARGET_STEP_DURATION = ( 1 / 60 ) / params.SIMULATION_STEPS_PER_FRAME;
+
 //
 
 function initLevel( levelID, familyID, world, ghosts ) {
@@ -32,28 +38,34 @@ function initLevel( levelID, familyID, world, ghosts ) {
 
 //
 
-const clock = new THREE.Clock();
-
 function gameLoop() {
 
-	const deltaTime = clock.getDelta();
+	const speedRatio = clock.getDelta() / TARGET_STEP_DURATION;
 
 	// move player
 
 	if ( player ) {
 
+		const targetVelY = ( Input.keys.top ? params.PLAYER_SPEED : 0 ) - ( Input.keys.bottom ? params.PLAYER_SPEED : 0 );
+		const targetVelX = ( Input.keys.right ? params.PLAYER_SPEED : 0 ) - ( Input.keys.left ? params.PLAYER_SPEED : 0 );
+
+		/*
 		if ( Input.keys.top ) player.pos.y += params.PLAYER_SPEED;
 		if ( Input.keys.bottom ) player.pos.y -= params.PLAYER_SPEED;
 		if ( Input.keys.right ) player.pos.x += params.PLAYER_SPEED;
 		if ( Input.keys.left ) player.pos.x -= params.PLAYER_SPEED;
+		*/
 
-		player.mesh.position.copy( player.pos );
+		playerVelocity.x += ( targetVelX - playerVelocity.x ) * 0.07;
+		playerVelocity.y += ( targetVelY - playerVelocity.y ) * 0.07;
+
+		player.mesh.position.add( playerVelocity );
 
 	}
 
 }
 
-setInterval( gameLoop, ( 1 / 60 ) / params.SIMULATION_STEPS_PER_FRAME );
+setInterval( gameLoop, TARGET_STEP_DURATION );
 
 //
 
